207fe1b595ec9d67ee7c7c2fd7260265649f5864
Trad Groover <gtg3vv@virginia.edu>
2018-04-19 17:22:35 -0400

    staff status

--- signup.php
+++ signup.php
@@ -42,7 +42,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     if(empty($username_err) && empty($password_err)){
         
         // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+        $sql = "INSERT INTO users (username, password, staff) VALUES ('$username', '$password', false)";
          
         if($result = mysqli_query($con, $sql)){
                 header("location: login.php");
