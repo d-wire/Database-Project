d07d99462046fe7815e0a27418f6ffb9c1f94d32
Trad Groover <gtg3vv@virginia.edu>
2018-04-21 18:41:50 -0400

    rest of the deletion stuff

--- search/creatureQuery.php
+++ search/creatureQuery.php
@@ -1,7 +1,7 @@
 <?php
 	require "dbutil.php";
 	$db = DbUtil::loginConnection();
-
+	session_start();
 	$stmt = $db->stmt_init();
 
 	if($_GET['species'] != '') {
@@ -12,7 +12,22 @@
 			$stmt->bind_result($actorID, $species);
 			echo "<table class='table' style='margin-right: 20px; margin-left: 20px;' border=1><th>ID</th><th>Species</th>\n";
 			while($stmt->fetch()) {
-				echo "<tr><td>$actorID</td><td>$species</td></tr>";
+			if($_SESSION['staff'] == 1)
+                        echo "<tr>\n
+                                <td>$actorID</td>\n
+                                <td>$species</td>\n
+                                <td>\n
+                                        <form action='./creatureSearch.php' method='post'>\n
+                                        <button type=submit id='species$actorID' name='actorID' value='$actorID' class='btn btn-danger itemBtn'>Delete</button>
+                                        </form>\n
+                                </td>\n
+                        </tr>";
+                 else
+                        echo "<tr>\n
+                                <td>$actorID</td>\n
+                                <td>$species</td>\n
+                        </tr>";
+
 			}
 			echo "</table>";
 
