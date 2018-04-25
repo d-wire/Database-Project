3b53627a076ae154f0c6fed23cf687f023e43e66
Liam Dwyer <ljd3za@virginia.edu>
2018-04-25 11:43:55 -0400

    Cleaned up some formatting

--- search/npcQuery.php
+++ search/npcQuery.php
@@ -10,13 +10,14 @@
 			$stmt->bind_param(s, $searchString);
 			$stmt->execute();
 			$stmt->bind_result($actorID, $name, $race, $bag);
-			echo "<table class='table' style='margin-right: 20px; margin-left: 20px;' border=1><th>ID</th><th>Name</th><th>Race</th>\n";
+			echo "<table class='table' style='margin-right: 20px; margin-left: 20px;' border=1><th>ID</th><th>Name</th><th>Race</th><th>Bag</th>\n";
 			while($stmt->fetch()) {
 				                 if($_SESSION['staff'] == 1)
                         echo "<tr>\n
                                 <td>$actorID</td>\n
                                 <td>$name <a class='btn btn-primary itemBtn pull-right' href='npc_result?id={$actorID}'>What Quest Do I Issue?</a></td>\n
                                 <td>$race</td>\n
+				<td>$bag</td>\n
                                 <td>\n
                                         <form action='./npcSearch.php' method='post'>\n
                                         <button type=submit id='name$actorID' name='actorID' value='$actorID' class='btn btn-danger itemBtn'>Delete</button>
@@ -28,6 +29,7 @@
                                 <td>$actorID</td>\n
                                 <td>$name <a class='btn btn-primary itemBtn pull-right' href='npc_result?id={$actorID}'>What Quest Do I Issue?</a></td>\n
                                 <td>$race</td>\n
+				<td>$bag</td>\n
                         </tr>";
 			}
 			echo "</table>";
@@ -42,13 +44,14 @@
 			$stmt->bind_param(s, $searchString);
 			$stmt->execute();
 			$stmt->bind_result($actorID, $name, $race, $bag);
-			echo "<table class='table' style='margin-right: 20px; margin-left: 20px;' border=1><th>ID</th><th>Name</th><th>Race</th>\n";
+			echo "<table class='table' style='margin-right: 20px; margin-left: 20px;' border=1><th>ID</th><th>Name</th><th>Race</th><th>Bag</th>\n";
 			while($stmt->fetch()) {
 				                 if($_SESSION['staff'] == 1)
                         echo "<tr>\n
                                 <td>$actorID</td>\n
                                 <td>$name <a class='btn btn-primary itemBtn pull-right' href='npc_result?id={$actorID}'>What Quest Do I Issue?</a></td>\n
                                 <td>$race</td>\n
+				<td>$bag</td>\n
                                 <td>\n
                                         <form action='./npcSearch.php' method='post'>\n
                                         <button type=submit id='race$actorID' name='actorID' value='$actorID' class='btn btn-danger itemBtn'>Delete</button>
@@ -60,6 +63,7 @@
                                 <td>$actorID</td>\n
                                 <td>$name <a class='btn btn-primary itemBtn pull-right' href='npc_result?id={$actorID}'>What Quest Do I Issue?</a></td>\n
                                 <td>$race</td>\n
+				<td>$bag</td>\n
                         </tr>";
 			}
 			echo "</table>";
